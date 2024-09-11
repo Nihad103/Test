3dddcbc5462894aac3fa93a8336d46cd26a2e17f
User <memmedlinihad103@gmail.com>
2024-09-11 11:56:20 +0300

    First Add

---
+++ app/src/main/java/com/example/movieapp/view/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.movieapp.view
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.movieapp.R
+import com.example.movieapp.adapter.MovieAdapter
+import com.example.movieapp.databinding.ActivityMainBinding
+import com.example.movieapp.viewmodel.MovieViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MovieViewModel
+    private lateinit var movieAdapter: MovieAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        prepareRecyclerView()
+        viewModel = ViewModelProvider(this)[MovieViewModel::class.java]
+        viewModel.getPopularMovies()
+        viewModel.observeMovieLiveData().observe(this, Observer { movieList ->
+            Log.e("movieList", "$movieList")
+            movieAdapter.setMovieList(movieList)
+
+        })
+    }
+
+    private fun prepareRecyclerView() {
+        movieAdapter = MovieAdapter()
+        binding.rvMovies.apply {
+            layoutManager = GridLayoutManager(applicationContext, 2)
+            adapter = movieAdapter
+        }
+    }
+}
